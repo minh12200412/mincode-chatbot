8fd30f3f2fe7263350f89c5bf2fe4f4f682c7a91
minh12200412~ <minhvuive122004@gmail.com>
2025-04-10 01:03:13 +0700

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import webRoutes from "./routers/web";
+require("dotenv").config();
+
+let app = express();
+
+viewEngine(app);
+webRoutes(app);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log("app is running at the port" + port);
+});
