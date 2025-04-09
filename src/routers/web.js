058f2a81676e22d12277129fd989ec002f68d3d7
minh12200412~ <minhvuive122004@gmail.com>
2025-04-10 01:56:47 +0700

    dev 2

--- src/routers/web.js
+++ src/routers/web.js
@@ -1,12 +1,13 @@
 import express from "express";
 import homeController from "../controllers/homeController.js";
 
-let router = express.Router();
-let initWebRouters = (app) => {
+const router = express.Router();
+
+const initWebRouters = (app) => {
   router.get("/", homeController.getHomePage);
   router.post("/webhook", homeController.postWebHook);
   router.get("/webhook", homeController.getWebHook);
   return app.use("/", router);
 };
 
-module.exports = initWebRouters;
+export default initWebRouters;
